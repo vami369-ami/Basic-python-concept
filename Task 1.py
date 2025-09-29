5999107e7ddbec456ae5b30cbd3a35d03d603cd1
vami369-ami <vami369@gmail.com>
2025-09-29 20:56:10 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,15 @@
+ # Task 1 : Basic Mathematical Operation
+A  = float(input("Enter first number: "))
+B  = float(input("Enter second number: "))
+
+# operations
+
+Addition = A + B
+Subtraction = A - B
+Multiplication = A * B
+Division = A / B
+
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
---
+++ Task 2.py
@@ -0,0 +1,7 @@
+# Task 2 : Create a personalized Greeting
+First_Name = input("Enter First Name: ")
+Second_Name  = input("Enter Second Name: ")
+
+Full_Name= First_Name + " " + Second_Name
+
+print("Hello,",Full_Name + "! Welcome to my world.")
